b6b15e2771faba989fe8256972f695011963c987
actatum88 <4946@holbertonschool.com>
2022-08-15 19:26:57 -0700

    Compiled and commenting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,13 +18,17 @@ void bubble_sort(int *array, size_t size)
 	while (swap == 1)
 	{
 		swap = 0;
+/*Check if swapping occurs. */
 		for (i = 1; i < size; i++)
+/*Loop to access each array element.*/
 		{
 			if (array[i] < array[i - 1])
+/*Compare two array elements to sort in descending order. */
 			{
 				temp = array[i];
 				array[i] = array[i - 1];
 				array[i - 1] = temp;
+/*Swapping occurs if elements are not in intended order. */
 				swap = 1;
 				print_array(array, size);
 			}
