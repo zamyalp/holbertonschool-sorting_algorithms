2fa11929b2c09cad0969dab1195cf8130f10aa33
zamya <zamyalp03@gmail.com>
2023-01-11 14:39:27 -0800

    sort merg

---
+++ 103-merge_sort.c
@@ -0,0 +1,33 @@
+function merge_sort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  var middle = Math.floor(array.length / 2);
+  var left = array.slice(0, middle);
+  var right = array.slice(middle);
+
+  return merge(merge_sort(left), merge_sort(right));
+}
+
+function merge(left, right) {
+  var result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    result.push(left.shift());
+  }
+
+  while (right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
+}
