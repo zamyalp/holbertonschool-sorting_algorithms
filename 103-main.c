82ad1d6a37e35fbcd79a82848db768a293b3da4f
zamya <zamyalp03@gmail.com>
2023-01-11 14:39:06 -0800

    main

---
+++ 103-main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    merge_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
+alex@/tmp/sort$ gcc -Wall -Wextra -Werror -pedantic 103-main.c 103-merge_sort.c print_array.c -o merge
+alex@/tmp/sort$ ./merge
+19, 48, 99, 71, 13, 52, 96, 73, 86, 7
+
+Merging...
+[left]: 19
+[right]: 48
+[Done]: 19, 48
+Merging...
+[left]: 71
+[right]: 13
+[Done]: 13, 71
+Merging...
+[left]: 99
+[right]: 13, 71
+[Done]: 13, 71, 99
+Merging...
+[left]: 19, 48
+[right]: 13, 71, 99
+[Done]: 13, 19, 48, 71, 99
+Merging...
+[left]: 52
+[right]: 96
+[Done]: 52, 96
+Merging...
+[left]: 86
+[right]: 7
+[Done]: 7, 86
+Merging...
+[left]: 73
+[right]: 7, 86
+[Done]: 7, 73, 86
+Merging...
+[left]: 52, 96
+[right]: 7, 73, 86
+[Done]: 7, 52, 73, 86, 96
+Merging...
+[left]: 13, 19, 48, 71, 99
+[right]: 7, 52, 73, 86, 96
+[Done]: 7, 13, 19, 48, 52, 71, 73, 86, 96, 99
+
+7, 13, 19, 48, 52, 71, 73, 86, 96, 99
