2d38c3323fd83951769f67ecfa65d4f2ff36c16d
Weirdo23 <98723905+zamyalp@users.noreply.github.com>
2023-02-01 16:29:47 -0600

    update

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic 0-bubble_sort.c 0-main.c print_array.c -o bubble
+gcc -Wall -Wextra -Werror -pedantic 0-bubble_sort.c 0-main.c print_array.c -o bubble 104-heap_sort.c
 echo 'compiled! :)'
